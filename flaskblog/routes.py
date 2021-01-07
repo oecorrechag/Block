220a711cb668c5d8f136a355187876ad8f6611a0
Oscar Correcha <oecorrechag@unal.edu.co>
2021-01-07 17:23:07 -0500

    README.md
    Modificaciones visuales

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -64,7 +64,7 @@ def save_picture(form_picture):
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
-    output_size = (125, 125)
+    output_size = (625, 625)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
